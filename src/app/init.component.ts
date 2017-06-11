20ecdd585c1c70205a08fc10b36ba1049345377f
NextTrick <ing.angeljara@gmail.com>
2017-06-11 18:22:23 -0500

    update

--- src/app/init.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'init-component',
-    template: '<router-outlet></router-outlet>'
-})
-export class InitComponent {
-
-}
--- src/typings.d.ts
+++
@@ -1,9 +0,0 @@
-/* SystemJS module definition */
-declare var module: NodeModule;
-
-declare var $:any;
-declare var jquery: any;
-
-interface NodeModule {
-  id: string;
-}
